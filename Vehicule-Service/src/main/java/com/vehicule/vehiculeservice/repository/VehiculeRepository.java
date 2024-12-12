ad3c11a02f60f34d97c9eade63f0e59bfe21f6a5
khadijachakkour <chakkourk4@gmail.com>
2024-12-12 19:54:40 +0100

    commit to save

--- Vehicule-Service/src/main/java/com/vehicule/vehiculeservice/repository/VehiculeRepository.java
+++ Vehicule-Service/src/main/java/com/vehicule/vehiculeservice/repository/VehiculeRepository.java
@@ -4,8 +4,6 @@ package com.vehicule.vehiculeservice.repository;
 import com.vehicule.vehiculeservice.entities.Vehicule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
 import java.util.List;
 
 @Repository
