7e18f38cd068064396e37f7f3c7673ab7ee3d973
elmekadem-narjiss <narjiss.elmekadem@gmail.com>
2024-12-20 16:54:02 +0100

    ADD Services

--- Security_Microservice/src/main/java/dcc/tp2/security_microservice/service/UserDetailService.java
+++ Security_Microservice/src/main/java/dcc/tp2/security_microservice/service/UserDetailService.java
@@ -15,53 +15,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
-@Service
-public class UserDetailService implements UserDetailsService {
-    private RestTemplate restTemplate;
 
-    public UserDetailService() {
-        this.restTemplate = new RestTemplate();
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String combinedUsername) throws UsernameNotFoundException {
-
-        String[] parts = combinedUsername.split(":");
-        if (parts.length != 2) {
-            throw new UsernameNotFoundException("Invalid username format. Expected format: email:type");
-        }
-
-        String email = parts[0];
-        String userType = parts[1];
-        System.out.println("DANS USER DETAIL ");
-        Map<String, String> user_infos = new HashMap<>();
-
-        if (userType.equals("Enseignant")){
-            System.out.println("YES Prof");
-            user_infos = restTemplate.getForObject("http://gateway-service:8888/ENSEIGNANT-SERVICE/Enseignants/email/"+email,HashMap.class);
-
-        }
-
-        if (userType.equals("Chercheur")){
-            System.out.println("valid");
-
-            user_infos = restTemplate.getForObject("http://gateway-service:8888/CHERCHEUR-SERVICE/Chercheurs/email/"+email,HashMap.class);
-        }
-
-
-        if (user_infos.isEmpty()){
-            new UsernameNotFoundException("User not found with username: " + email);
-            System.out.println(" AUcun user");
-        }
-
-        Collection<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(user_infos.get("scope")));
-
-        return new User(user_infos.get("email"), "{noop}"+user_infos.get("password"), authorities);
-    }
-
- */
 @Service
 public class UserDetailService implements UserDetailsService {
     private RestTemplate restTemplate;
