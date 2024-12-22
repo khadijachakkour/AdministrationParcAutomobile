ed7ebf69d39b2b047109cdcf08d96a5d2df9c98a
khadijachakkour <chakkourk4@gmail.com>
2024-12-22 23:29:15 +0100

    Added CORS Configuration

---
+++ Vehicule-Service/src/main/java/com/vehicule/vehiculeservice/configuration/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.vehicule.vehiculeservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**")
+                        .allowedOrigins("http://localhost:4200") // Remplacez par l'URL de votre frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
